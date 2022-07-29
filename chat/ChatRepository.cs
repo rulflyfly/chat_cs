ac7b7ae2e8e8dae966ce0e3bfce80d69d8699038
Anastasia Popova <rulflyfly@gmail.com>
2022-07-29 22:23:52 +0300

    change file path

--- chat/ChatRepository.cs
+++ chat/ChatRepository.cs
@@ -5,7 +5,7 @@ namespace chat
 {
     public class ChatRepository
     {
-        public static readonly string filePath = "/Users/Nastya/Documents/learning/С#/chat/chat/data/chat-data.json";
+        public static readonly string filePath = "/Users/Nastya/Documents/learning/cs/chat/chat/data/chat-data.json";
 
         public static Chat ReadChatData()
         {
