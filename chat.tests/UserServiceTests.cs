d919202a1f7bafde9e0e56ddd0261def5c2f32c1
menswear <MAZAKSIM@outlook.com>
2022-08-08 11:09:06 +0800

    UserServiceTests and user-data

---
+++ chat.tests/UserServiceTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using chat;
+using chat.domain;
+using Microsoft.VisualStudio.TestPlatform;
+
+
+namespace chat.tests
+{
+    
+    public class UserServiceTests
+    {
+        // GetAllUsers
+        [Test]
+        public void GetsAllUsersFromOriginalDataBase_expected_5()
+        {
+            var expected = 5;
+            
+            var actual = UserService.GetAllUsers().Count();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetAllUsers_AllUsersContainsUserWithSameProperties()
+        { 
+            var user2 = new User(4032);
+            user2.Name = "Suhasya";
+            user2.Birthday = "08/01/2020";
+
+            var actual = UserService.GetAllUsers()[1];
+
+            Assert.AreEqual(user2, actual);
+        }
+
+        // GetUserById
+        [Test]
+        public void FunctionGetsNumber_OneOfUserIdEqualsNumber()
+        {
+            var list = UserService.GetAllUsers();
+            var users = list.ToArray();
+            var number = 4032;
+
+            var actual = UserService.GetUserById(number);
+
+            CollectionAssert.Contains(users, actual);
+        }
+
+        // GetUserByName
+        [Test]
+        public void CheckUsersInput_ListOfUsersContainPropertyWithSameName()
+        {
+            var userInput = "Test_2";
+
+            var actual = UserService.GetUserByName(userInput).Name;
+
+            Assert.AreEqual(actual, userInput);
+        }
+
+        [Test]
+        public void CheckUsersInputIsNULL_ListOfUsersContainPropertyWithSameName()
+        {
+            var actual = "";
+
+            var expected = UserService.GetUserByName(actual);
+
+            //Assert.AreEqual(actual, expected);
+            Assert.Null(expected);
+        }
+
+        // SingUpUser
+        [Test]
+        public void NewUsersInputsNameAndBirth_users_dataContainsNewUserWithGivenParameters()
+        {
+            var name = "TestUser124";
+            var bday = "02/02/2004";
+            int[] arr = Enumerable.Range(0, 9999 + 1).ToArray();
+            var expectedUserName = "TestUser124";
+            var expectedUserBirthday = "02/02/2004";
+
+            var actual = UserService.SingUpUser(name, bday);
+
+            Assert.AreEqual(actual.Name, name);
+            Assert.AreEqual(actual.Birthday, bday);
+            CollectionAssert.Contains(arr, actual.Id);
+
+        }
+
+
+
+    }
+}
+
