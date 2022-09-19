0619a1b976fecca0c6fe012124876b48a1c12f0d
Anastasia Popova <rulflyfly@gmail.com>
2022-09-19 14:07:06 +0300

    add chat controllers

---
+++ ChatAPI/Controllers/ChatController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using chat.domain;
+using ChatAPI.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ChatController : Controller
+    {
+        private IChatRepository _chatRepository;
+
+        public ChatController(IChatRepository chatRepository)
+        {
+            _chatRepository = chatRepository;
+        }
+
+        // GET: api/chat/1
+        [HttpGet("{id}")]
+        public List<Message> GetMessagesVisibleToUser(int id, [FromBody] User user)
+        {
+            var chats = _chatRepository.ReadChatData();
+
+            var chat = ChatApiUtils.FilterChatsById(chats, id);
+
+            if (chat == null) return null;
+
+            return chat.GetMessagesVisibleToUser(user);
+        }
+    }
+}
+
---
+++ ChatAPI/Controllers/ChatServiceController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using chat.domain;
+using ChatAPI.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ChatAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ChatServiceController : Controller
+    {
+
+        private IChatRepository _chatRepository;
+
+        public ChatServiceController(IChatRepository chatRepository)
+        {
+            _chatRepository = chatRepository;
+        }
+
+        // GET api/chatservice/1/1
+        [HttpGet("{chatId}/{userId}")]
+        public List<Message> GetAllMessages(int chatId, int userId)
+        {
+            var chats = _chatRepository.ReadChatData();
+
+            var chat = ChatApiUtils.FilterChatsById(chats, chatId);
+
+            if (chat == null) return null;
+
+            var chatService = new ChatService();
+
+            return chatService.GetAllMessages(Convert.ToDouble(userId), chat);
+        }
+
+        // GET api/chatservice/search/1/1
+        [HttpGet("search/{chatId}/{userId}")]
+        public List<Message> GetUserMessages(int chatId, int userId, [FromBody] string searchName)
+        {
+            var chats = _chatRepository.ReadChatData();
+
+            var chat = ChatApiUtils.FilterChatsById(chats, chatId);
+
+            if (chat == null) return null;
+
+            var chatService = new ChatService();
+
+            return chatService.GetUserMessages(Convert.ToDouble(userId), searchName, chat);
+        }
+    }
+}
+
