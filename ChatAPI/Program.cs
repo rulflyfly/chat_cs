5cebedf7bc0d85a0138ee19cb2255807283f5d1d
menswear <MAZAKSIM@outlook.com>
2022-09-23 14:06:27 +0700

    new controller methods

--- ChatAPI/Program.cs
+++ ChatAPI/Program.cs
@@ -9,6 +9,9 @@ builder.Services.AddControllers();
 builder.Services.AddSingleton<IChatService, ChatService>();
 builder.Services.AddSingleton<IChatRepository, ChatRepository>();
 builder.Services.AddSingleton<IUserRepository, UserRepository>();
+builder.Services.AddSingleton<ILikeService, LikeService>();
+builder.Services.AddSingleton<IUserService, UserService>();
+
 
 var app = builder.Build();
 
--- chat/domain/IChatService.cs
+++ chat/domain/IChatService.cs
@@ -4,5 +4,6 @@
     {
         List<Message> GetAllMessages(int userId, int chatId);
         List<Message> GetUserMessages(int chatId, int userId, string searchName);
+        void WriteMessage(int chatId, int userId, string text);
     }
 }
